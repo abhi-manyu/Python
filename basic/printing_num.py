075bee474050c386257f9d69a3dcc59811f37678
abhi-manyu <abhim.moharana@gmail.com>
2019-11-29 09:20:59 +0530

    how to print new output without going to new line or in same line
    just add end='' into ur printing statement

---
+++ basic/printing_num.py
@@ -0,0 +1,13 @@
+counter=input("enter the number , upto which u want to see the list")
+number=1
+while(number<=counter):
+    print(number, end='')
+    number=number+1
+
+#here the end='' represents that there wouldn't be any ',' or new line between any of the printing statements inside that loop
+#that means it will come as 12345....in stead of 1
+               #                                 2
+                                #                3
+                                #                4
+                                #                .
+                                #                . like this
